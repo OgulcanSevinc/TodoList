073606e8e1ee9ec284fba766bfb29e156545cd0e
Ogulcan Sevinç <ogulcan.sevinc@student.hepl.be>
2024-09-24 18:12:32 +0200

    feature:use component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
 <form action="" @submit.prevent="addTodo">
   <fieldset role="group">
     <input
@@ -12,25 +11,30 @@
   <div v-if="todos.length == 0"> Vous n'avez pas de tâche à faire :(</div>
   <div v-else>
     <ul>
-      <li v-for="todo in sortedTodo()"
+      <li v-for="todo in sortedTodo"
       :key="todo.date"
       :class="{completed: todo.completed}"
       >
-      <label>
-        <input type="checkbox" v-model="todo.completed">
-        {{ todo.title  }}
-      </label>
+      <Checkbox :label="todo.title" 
+      @check="console.log('coché')" 
+      @uncheck="console.log('décoché')" 
+      />
     </li>
     </ul>
     <label>
       <input type="checkbox" v-model="hideCompleted">
       Masquer les tâches complétées
     </label>
+    <p v-if="remainingTodos > 0">
+        {{ remainingTodos }} tâche{{ remainingTodos > 1 ? 's' : '' }} à faire
+      </p>
+      <Checkbox :label="'Checkbox composant dynamique'"/>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { computed, ref } from 'vue';
+import Checkbox from './Checkbox.vue'
 
 const todos = ref([{
   title: 'Tâche de test',
@@ -57,17 +61,18 @@ const addTodo = () => {
   newTodo.value= ''
 }
 
-const sortedTodo = () => {
+const sortedTodo = computed(() => {
   const sortedTodos = todos.value.toSorted((a,b) => a.completed > b.completed ? 1 : -1)
   if(hideCompleted.value == true){
     return sortedTodos.filter(t => t.completed == false )
   }
   return sortedTodos
   
-}
+})
 
-
-
+const remainingTodos = computed(() => {
+  return todos.value.filter(t => t.completed == false).length
+})
 </script>
 
 <style>
